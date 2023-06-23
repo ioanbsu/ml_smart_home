1e3ca6158b8efea39e925218e4bd84a46c3d54ec
ioanbsu <ibah@google.com>
2023-06-23 10:56:20 -0700

    - minor conda env improvements.

--- nuke_and_setup_from_scratch.sh
+++ nuke_and_setup_from_scratch.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env sh
-#executing: ./full_env_reset.sh env_name
+#executing: ./nuke_and_setup_from_scratch.sh env_name
 # replace env_name with env name of your choice.
 env_name=$1
 resolved_env=${env_name:-m1_gpu_env}
 
+# If you're seeing issues installing som eof packages, your conda env might have cached files that may worth cleaning up
+# uncomment line below if this is the case.
+#conda clean --all
+conda update conda
+conda update python
 conda deactivate
 conda env remove -n ${resolved_env}
 conda env create -f tf-metal-arm64.yaml -n ${resolved_env}
 conda init zsh
 
 
-echo "Conda env setup completed. To activate your environment run `conda activate ${resolved_env}`"
+echo "Conda env setup completed. To activate your environment run \`conda activate ${resolved_env}\`"
 echo "once activating the env, run following cmds: "
 echo "pip install numpy  --upgrade"
 echo "pip install pandas  --upgrade"
@@ -18,5 +23,5 @@ echo "pip install matplotlib  --upgrade"
 echo "pip install scikit-learn  --upgrade"
 echo "pip install scipy  --upgrade"
 echo "pip install plotly  --upgrade"
-echo "jupyter-lab"
+echo "pip install ultralytics  --upgrade"
 
